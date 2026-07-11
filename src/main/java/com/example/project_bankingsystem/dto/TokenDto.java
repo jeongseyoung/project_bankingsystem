9f00b618b9c01ebb403dd71a79b8a83e4e9dbd10
jeongseyoung <sengangel@naver.com>
2026-07-11 23:37:11 +0900

    Update TokenDto.java

--- src/main/java/com/example/project_bankingsystem/dto/TokenDto.java
+++ src/main/java/com/example/project_bankingsystem/dto/TokenDto.java
@@ -9,7 +9,7 @@ public class TokenDto {
     private String accessToken;
     private String refreshToken;
     private String tokenType = "Bearer ";
-
+   
     public TokenDto(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
